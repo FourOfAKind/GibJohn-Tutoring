105f3954a7a8f0b4929d0cf75b3d249a684c58bb
FourOfAKind <321646@edu.mkcollege.ac.uk>
2023-11-24 14:17:39 +0000

    Implement notes model

---
+++ GibJohn/Migrations/20231124141714_NoteInit.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GibJohn.Migrations
+{
+    public partial class NoteInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- GibJohn/Models/Note.cs
+++ GibJohn/Models/Note.cs
@@ -1,6 +1,17 @@
-﻿namespace GibJohn.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GibJohn.Models
 {
     public class Note
     {
+        public int Id { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Must be between 5 and 50 characters.", MinimumLength = 5)]
+        public string Topic { get; set; }
+        [Required]
+        [StringLength(250, ErrorMessage = "Must be between 5 and 250 characters.", MinimumLength = 5)]
+        public string Notes { get; set; }
     }
 }

03e627da035d214bd556a3b9df5b64fb3de8a153
Jamie Hodgson <321646@edu.mkcollege.ac.uk>
2023-11-29 17:06:02 +0000

    Misc changes

--- GibJohn/Models/Course.cs
+++ GibJohn/Models/Course.cs
@@ -11,10 +11,10 @@ namespace GibJohn.Models
         [Key]
         public int Id{ get; set; }
         [Required]
-        [StringLength(50, MinimumLength = 2)]
+        [StringLength(50, MinimumLength = 5)]
         public string Name { get; set; }
         [Required]
-        [StringLength(250, MinimumLength = 2)]
+        [StringLength(250, MinimumLength = 5)]
         public string Description { get; set; }
         [AllowNull]
         public string? ImageURL { get; set; }

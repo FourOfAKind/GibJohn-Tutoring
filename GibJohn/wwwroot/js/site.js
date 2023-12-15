1bd3f871aec692829cec84dc8848fb9da1339a47
FourOfAKind <mrjamshodgson@gmail.com>
2023-12-15 22:59:43 +0000

    Allow for

--- GibJohn/wwwroot/js/site.js
+++ GibJohn/wwwroot/js/site.js
@@ -4,15 +4,22 @@
 }
 
 function getTheme() {
-    localStorage.getItem("theme");
+    let currentTheme;
+    if (localStorage.length === 0) {
+        currentTheme = setTheme("dark");
+    } else {
+        currentTheme = localStorage.getItem('theme');
+    }
+    return currentTheme;
+
 }
 
 function applyTheme() {
-    let theme = getTheme();
-    if (theme === "dark") {
-        document.documentElement.setAttribute("data-bs-theme", "dark");
+    var theme = getTheme();
+    if (theme === "light") {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
     }
-    else {
-        document.documentElement.setAttribute("data-bs-theme", "light");
+    else if (theme === "dark") {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
     }
 }

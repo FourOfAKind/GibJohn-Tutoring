c98825abf294f8d696728f81eca8d72f9e40904b
FourOfAKind <mrjamshodgson@gmail.com>
2023-12-16 23:30:05 +0000

    fix DB issues

---
+++ GibJohn/Migrations/20231216232619_init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GibJohn.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CourseLevel = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ExamBoard = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Request");
+        }
+    }
+}

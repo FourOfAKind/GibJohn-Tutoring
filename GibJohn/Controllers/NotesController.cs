9062ef6f1fee0015455a7cf880856dda2511790a
FourOfAKind <mrjamshodgson@gmail.com>
2023-12-18 16:40:44 +0000

    Notes modification

--- GibJohn/Controllers/NotesController.cs
+++ GibJohn/Controllers/NotesController.cs
@@ -113,7 +113,7 @@ namespace GibJohn.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,CourseId,Topic,Notes")] Note note)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CourseId,Topic,Notes,Video")] Note note)
         {
             if (id != note.Id)
             {

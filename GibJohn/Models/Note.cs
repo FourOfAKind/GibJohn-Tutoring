15aec9539bebf568a67bdec0902f698aa7ce8537
FourOfAKind <mrjamshodgson@gmail.com>
2023-12-20 00:44:15 +0000

    Allow video to be null (woops)

--- GibJohn/Models/Note.cs
+++ GibJohn/Models/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GibJohn.Models
 {
@@ -17,6 +18,7 @@ namespace GibJohn.Models
         [Required]
         [StringLength(2000, ErrorMessage = "Must be between 5 and 250 characters.", MinimumLength = 5)]
         public string Notes { get; set; }
+        [AllowNull]
         [StringLength(150, ErrorMessage = "Must be between 15 and 150 characters.", MinimumLength = 15)]
         public string Video { get; set; }
     }

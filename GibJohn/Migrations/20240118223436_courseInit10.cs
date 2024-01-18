675a9ec1b297f947d45f15be4a6d8dbbbae69777
FourOfAKind <mrjamshodgson@gmail.com>
2024-01-18 22:40:37 +0000

    implement image upload backend (implement frontend tomorrow)

---
+++ GibJohn/Migrations/20240118223436_courseInit10.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GibJohn.Migrations
+{
+    public partial class courseInit10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Video",
+                table: "Note",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Course");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Video",
+                table: "Note",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
